2e7c0ea77a4f7ceba282c29fac16061bb5d4e464
Matthijs <matthijsvaneijk@gmail.com>
2020-02-07 13:50:01 +0100

    kale velden toegevoegd, geen validatie en submitted waarden worden ontvangen en logged

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+  loginForm: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]),
+      password: new FormControl('', [Validators.required])
+    });
   }
 
+  onSubmit() {
+    const value = this.loginForm.value;
+    console.log(value);
+  }
 }
